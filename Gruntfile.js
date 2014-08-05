c5af5461c9e1d72efc31dc2ed892e174158d0aaa
Manolo <manolo@plyfe.me>
2014-08-05 15:52:26 -0400

    Adds branch release support

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,7 @@ module.exports = function(grunt) {
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo: 'components',
+        pushTo: 'origin',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
       }
     },
